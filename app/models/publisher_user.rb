fb688a13bbd0565217a843cf9ba818eeaa1993e9
lemonjai <leocheung_1986@hotmail.com>
2017-06-15 20:54:07 -0400

    initialized and add the devise gem

---
+++ app/models/publisher_user.rb
@@ -0,0 +1,6 @@
+class PublisherUser < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
---
+++ test/models/publisher_user_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PublisherUserTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
