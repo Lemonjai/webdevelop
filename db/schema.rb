ba0d1c04e8a2f2d68aa382a82ca7346a1bd17dfe
lemonjai <leocheung_1986@hotmail.com>
2017-06-20 08:57:17 -0400

    add pages controller, model and active_admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170616121233) do
+ActiveRecord::Schema.define(version: 20170620125439) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,6 +78,13 @@ ActiveRecord::Schema.define(version: 20170616121233) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_developer_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_developer_users_on_reset_password_token", unique: true
+  end
+
+  create_table "pages", force: :cascade do |t|
+    t.string "name"
+    t.text "context"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "publisher_users", force: :cascade do |t|
